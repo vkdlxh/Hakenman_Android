c0e14ea42b153b76238f34015827d62742e7e946
vkdlxh <tkdwnszoq@naver.com>
2019-10-06 00:19:52 +0900

    バグホットフィックス 1.1.1

--- app/src/main/java/archiveasia/jp/co/hakenman/Manager/WorksheetManager.kt
+++ app/src/main/java/archiveasia/jp/co/hakenman/Manager/WorksheetManager.kt
@@ -252,7 +252,7 @@ object WorksheetManager {
     private fun createDate(year: Int, month: Int, day: Int): Date {
         val cal = Calendar.getInstance()
         cal.set(Calendar.YEAR, year)
-        cal.set(Calendar.MONTH, month)
+        cal.set(Calendar.MONTH, month - 1)
         cal.set(Calendar.DAY_OF_MONTH, day)
         cal.set(Calendar.HOUR_OF_DAY, 0)
         cal.set(Calendar.MINUTE, 0)
