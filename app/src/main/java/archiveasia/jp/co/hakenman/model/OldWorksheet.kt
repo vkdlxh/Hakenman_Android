66fd8fc03f73b3785832c69d7d56666d77c43518
vkdlxh <jeonsangjun@archive-asia.co.jp>
2020-06-14 21:23:59 +0900

    アップデート後アプリが落ちる不具合対応

--- app/src/main/java/archiveasia/jp/co/hakenman/model/OldWorksheet.kt
+++ app/src/main/java/archiveasia/jp/co/hakenman/model/OldWorksheet.kt
@@ -1,23 +1,38 @@
 package archiveasia.jp.co.hakenman.model
 
+import com.google.gson.annotations.SerializedName
 import java.util.*
 
 data class OldWorksheet (
+        @SerializedName("b")
         var workDate: Date,                             // 勤務日時
+        @SerializedName("c")
         var workTimeSum: Double,                        // 勤務時間合計
+        @SerializedName("d")
         var workDaySum: Int,                            // 勤務日合計
+        @SerializedName("e")
         var detailWorkList: MutableList<OldDetailWork>  // 詳細勤務情報
 )
 
 data class OldDetailWork (
+        @SerializedName("a")
         val workYear: Int,              // 年
+        @SerializedName("b")
         val workMonth: Int,             // 月
+        @SerializedName("c")
         val workDay: Int,               // 日
+        @SerializedName("d")
         val workWeek: String,           // 週
+        @SerializedName("e")
         var workFlag: Boolean,          // 勤務フラグ
+        @SerializedName("f")
         var beginTime: Date? = null,    // 出社時間
+        @SerializedName("g")
         var endTime: Date? = null,      // 退社時間
+        @SerializedName("h")
         var breakTime: Date? = null,    // 休憩時間
+        @SerializedName("i")
         var duration: Double? = null,   // 勤務時間
+        @SerializedName("j")
         var note: String? = null        // 備考
 )
--- app/src/main/java/archiveasia/jp/co/hakenman/model/Worksheet.kt
+++ app/src/main/java/archiveasia/jp/co/hakenman/model/Worksheet.kt
@@ -2,13 +2,18 @@ package archiveasia.jp.co.hakenman.model
 
 import android.os.Parcel
 import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
 import kotlinx.android.parcel.Parcelize
 import java.util.*
 
 data class Worksheet (
+        @SerializedName("workDate")
         var workDate: Date,                         // 勤務日時
+        @SerializedName("workTimeSum")
         var workTimeSum: Double,                    // 勤務時間合計
+        @SerializedName("workDaySum")
         var workDaySum: Int,                        // 勤務日合計
+        @SerializedName("detailWorkList")
         var detailWorkList: MutableList<DetailWork> // 詳細勤務情報
 ): Parcelable {
 
@@ -48,11 +53,18 @@ data class Worksheet (
 
 @Parcelize
 data class DetailWork (
+        @SerializedName("workDate")
         val workDate: Date,             // 勤務日
+        @SerializedName("workFlag")
         var workFlag: Boolean,          // 勤務フラグ
+        @SerializedName("beginTime")
         var beginTime: Date? = null,    // 出社時間
+        @SerializedName("endTime")
         var endTime: Date? = null,      // 退社時間
+        @SerializedName("breakTime")
         var breakTime: Date? = null,    // 休憩時間
+        @SerializedName("duration")
         var duration: Double? = null,   // 勤務時間
+        @SerializedName("note")
         var note: String? = null        // 備考
 ): Parcelable
